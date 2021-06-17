8547f488ace014c03b15d44b3b9df5ac621b6021
Lilfjjdb <86081096+Lilfjjdb@users.noreply.github.com>
2021-06-17 18:00:16 -0300

    Add files via upload

--- ULTRA/plugins/help.py
+++ ULTRA/plugins/help.py
@@ -1,61 +1,45 @@
-# -*- coding: UTF-8 -*-
-import os
-admin = os.getenv("ADMIN")
-def help_user(id,first_name):
-	if id == int(admin):
-		msg = f"""
-[ HELP ]
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+"""Userbot help command"""
 
-------------------------------
+import asyncio
+from userbot import ALIVE_NAME, CMD_HELP
+from userbot.events import register
+from platform import uname
 
-+ <b>start</b> : start the bot
+modules = CMD_HELP
 
-+ <b>search</b> : search webs
+# Ported by KENZO (Lynx-Userbot)
+# ================= CONSTANT =================
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+# ============================================
 
-+ <b>iban</b>: verify iban
 
-+ <b>bin</b> : verify bin
-
-+ <b>nbin</b> : create new bin
-
-+ <b>niban</b>: create new iban
-
-+ <b>gen</b> : generate from bin
-
-+ <b>li</b> : linux command handling
-
-+ <b>admin_web</b> : find admin panel
-
-+ <b>info</b> : view user information
-
-------------------------------
-
-[ OWNER : <code>{first_name}</code> ]"""
-	else:
-		msg = f"""
-[ HELP ]
-
-------------------------------
-
-+ <b>start</b> : start the bot
-
-+ <b>search</b> : search webs
-
-+ <b>iban</b> : verify iban
-
-+ <b>bin</b> : verify bin
-
-+ <b>nbin</b> : create new bin
-
-+ <b>niban</b> : create new iban
-
-+ <b>gen</b> : generate from bin
-
-+ <b>admin_web</b> : find admin panel
-
-+ <b>info</b> : view user information
-
-------------------------------
-
-[ USER : <code>{first_name}</code> ]"""
-	return msg
+@register(outgoing=True, pattern="^.help(?: |$)(.*)")
+async def help(event):
+    """For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("**Plugin yang anda ketik salah ❌\nMohon ketik plugin dengan benar.**")
+            await asyncio.sleep(200)
+            await event.delete()
+    else:
+        string = ""
+        for i in CMD_HELP:
+            string += "`" + str(i)
+            string += "`\t|  "
+        await event.edit("⚡")
+        await asyncio.sleep(3)
+        await event.edit("**⚡𝗟𝘆𝗻𝘅-𝙐𝙎𝙀𝙍𝘽𝙊𝙏⚡**\n\n"
+                         f"**◑» Bᴏᴛ ᴏꜰ {DEFAULTUSER}**\n**◑» Pʟᴜɢɪɴ : {len(modules)}**\n\n"
+                         "**• Mᴀɪɴ Mᴇɴᴜ :**\n"
+                         f"╰►| {string} ◄─\n\n")
+        await event.reply(f"\n**Contoh** : Ketik » `.help busy` Untuk Informasi Pengunaan Plugin Busy.\nAtau Bisa Juga Dengan Cara, Ketik `.helpme` Untuk Menggunakan Inline Bot Dari @BotFather.\n Jika Tidak Tahu Caranya, Silahkan Bertanya ke » [sini](t.me/SyndicateTwenty4) « Terimakasih 🙏")
+        await asyncio.sleep(1000)
+        await event.delete()
